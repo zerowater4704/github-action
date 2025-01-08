b76e407f7962ea6ab12b25935a9f74f8ef0b45c9
KimYoungSoo <zerowater4704@gmail.com>
2025-01-08 22:44:14 +0900

    ec2とのdockerの接続確認

---
+++ backend/dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+dotenv_1.default.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+const nodeEnv = process.env.NODE_ENV;
+console.log(`현재 실행 환경: ${nodeEnv}`);
+app.get("/", (req, res) => {
+    res.send("Hello Docker");
+});
+app.listen(PORT, () => {
+    console.log("server is running");
+});
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = 3000;
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+const nodeEnv = process.env.NODE_ENV;
+
+console.log(`현재 실행 환경: ${nodeEnv}`);
 
 app.get("/", (req, res) => {
   res.send("Hello Docker");
